7ef94492c65e335400726d1e32a464f96e51c24e
abrar-sde <mohammedabrar3218@gmail.com>
2024-07-25 13:26:12 +0530

    App working fine

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # API routes should be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      get 'search/posts'
+      resources :posts
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+end
